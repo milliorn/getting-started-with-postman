d60a9fd247846f155c4eeeb3dd814cb63aad87d9
milliorn <scottmilliorn@gmail.com>
2023-02-01 13:58:24 -0800

    Send system information

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import { WebSocketServer } from "ws";
+import si from "systeminformation";
 
-const wss = new WebSocketServer({ port: 8080 });
+// Set up a WebSockets server in Node.js
+const webSocketServer = new WebSocketServer({ port: 8080 });
+const milliseconds = 60000;
 
-wss.on("connection", (ws) => {
-  ws.on("message", (data) => {
+webSocketServer.on("connection", (websocket) => {
+  websocket.on("message", (data) => {
     console.log("received: %s", data);
   });
-  ws.send("something");
+  websocket.send(`Sending system information every ${milliseconds}ms.`);
+
+  setInterval(async () => {
+    const cpuTemp = JSON.stringify(await si.currentLoad());
+    websocket.send(cpuTemp);
+  }, 60000);
 });
+// end
